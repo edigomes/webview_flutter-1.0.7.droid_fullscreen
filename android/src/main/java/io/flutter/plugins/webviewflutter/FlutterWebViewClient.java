ff2e70941c1c65c0e34330415fe02414ac67e8e5
lozinskydino <adso.menezes@gmail.com>
2021-04-22 03:50:28 -0300

    Update FlutterWebViewClient.java

--- android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebViewClient.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebViewClient.java
@@ -192,10 +192,10 @@ class FlutterWebViewClient {
                       request.getUrl().toString().contains("shaidolt") ||
                       request.getUrl().toString().contains("stawhoph") ||
                       request.getUrl().toString().contains("analytics")
-                      //request.getUrl().toString().contains("heeteefu") ||
-                      //request.getUrl().toString().contains("push") ||
-                      //request.getUrl().toString().contains("jomtingi") ||
-                      //request.getUrl().toString().contains("denetsuk") ||
+                      request.getUrl().toString().contains("heeteefu") ||
+                      request.getUrl().toString().contains("push") ||
+                      request.getUrl().toString().contains("jomtingi") ||
+                      request.getUrl().toString().contains("denetsuk") ||
                       //request.getUrl().toString().contains("fonts") ||
                       //request.getUrl().toString().contains("psaughun") ||
                       //request.getUrl().toString().contains("jomtingi") ||
